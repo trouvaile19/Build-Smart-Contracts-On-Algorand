f71f16f54abb7498324b20b432efab655617a298
Phucpham <phamthanhqb2005@gmail.com>
2024-07-27 14:35:30 +0700

    add comments for contract.py

--- First_SmartContract/les2_compile/projects/les2_compile/smart_contracts/les_compile/contract.py
+++ First_SmartContract/les2_compile/projects/les2_compile/smart_contracts/les_compile/contract.py
@@ -1,35 +1,44 @@
+# Create, update a Smart Contract
 from algopy import ARC4Contract, arc4, UInt64, Asset, Global, Txn, gtxn, itxn
 from algopy.arc4 import abimethod
-global
+# global
 # Txn - transaction
 # gtxn - global transaction 
-# ilxn -
+# ilxn - interface transaction
 
 class LesCompile(ARC4Contract):
     # local storage
-    asset_id: UInt64
+    asset_id: UInt64  
     price: UInt64
     
     @arc4.abimethod(create="require", allow_actions = ["NoOp"])
-    def create_application(self, asset_id: Asset, price: UInt64) -> None:
+    def create_application(self, asset_id: Asset, price: UInt64) -> None: #no return
         self.asset_id = asset_id.id
         self.price = price
 
     @arc4.abimethod
     def update_price(self, new_price: UInt64) -> UInt64:
-        assert Txn.sender == Global.creator_address
-
+        # Check that txn sender (Txn.sender) must match the creator txn (creator_address)   
+        assert Txn.sender == Global.creator_address #'Global' variable
+        # update new price
         self.price = new_price
         return new_price
     
     @arc4.abimethod
-    def buy(self, buyer_txn: gtxn.PaymentTransaction, quality: UInt64) -> None:
-            assert self.price != 0
-            assert Txn.sender == buyer_txn.sender
-            assert buyer_txn.amount == self.price * quality // 10 *20 
+    # buyer_txn to storage an address & gtxn on BC
+    def buy(self, buyer_txn: gtxn.PaymentTransaction, quanity: UInt64) -> None: 
+            assert self.price != 0      #check valid money
+            assert Txn.sender == buyer_txn.sender    #check valid sender
+            assert buyer_txn.amount == self.price * quanity // 10 *20  #chẹc
             
             itxn.AssetTransfer(
-                 xfer_asset=self.asset_id,
-                 asset_receiver= Txn.sender
-                 asset_amount=quaility
+                 xfer_asset=self.asset_id,   # retrieve asset_id
+                 asset_receiver= Txn.sender,     # send to buyer (asset_receiver 
+                 asset_amount=quanity,
             ).submit()
+
+# The functions within the contract will always be passed 'self' (ARC4Contract).
+# @arc4.abimethod to definite SC methods for Algorand (), save method on SC.
+# Check that txn sender (Txn.sender) must match the creator txn (creator_address),
+# so that we can avoid being adjusting contract by any people who know asset_id.
+# gtxn retrieves data from the global transaction to synchronize with all nodes on BC
