4e94f36f0f55b7bd083a203ce4de3a01a5c183f0
Phucpham <phamthanhqb2005@gmail.com>
2024-08-15 14:22:20 +0700

    feat: update contract & Test

--- adv-marketplace/projects/adv-marketplace-contracts/smart_contracts/adv_marketplace/contract.py
+++ adv-marketplace/projects/adv-marketplace-contracts/smart_contracts/adv_marketplace/contract.py
@@ -1,9 +1,13 @@
 from algopy import ARC4Contract, Account, Asset, Global, String, Txn, UInt64, gtxn, itxn, arc4, op, subroutine
 from algopy.arc4 import abimethod
 
-BOX_LENGTH_FOR_SALE = 64
-MINIMUM_FEE_FOR_SALE = 2_500 + 400 + BOX_LENGTH_FOR_SALE
-
+FOR_SALE_BOX_KEY_LENGTH = 48
+FOR_SALE_BOX_VALUE_LENGTH = 64
+# Create box -> 0.0025 algos (fee)
+# byte per box -> 0.0004 algos (fee)
+FOR_SALE_BOX_SIZE = FOR_SALE_BOX_KEY_LENGTH + FOR_SALE_BOX_VALUE_LENGTH
+FOR_SALE_BOX_MBR = 2_500 + FOR_SALE_BOX_SIZE * 400
+# d, "MBR":  Minimum Balance Requirement
 class AdvMarketplace(ARC4Contract):
     # opt-into-access (co nhieu access hon)
     # allow_access
@@ -11,14 +15,28 @@ class AdvMarketplace(ARC4Contract):
     # first_deposit
     # withdraw
     # buy
-    
+    @subroutine
+    def quantity_price(
+        self, quantity: UInt64, price: UInt64, asset_decimals: UInt64
+    ) -> UInt64:
+        amount_not_scaled_high, amount_not_scaled_low = op.mulw(price, quantity)
+        scaling_factor_high, scaling_factor_low = op.expw(10, asset_decimals)
+        _quotient_high, amount_to_be_paid, _remainder_high, _remainder_low = op.divmodw(
+            amount_not_scaled_high,
+            amount_not_scaled_low,
+            scaling_factor_high,
+            scaling_factor_low,
+        )
+        assert not _quotient_high
+
+        return amount_to_be_paid
 
     @abimethod()
-    def allow_access(self, name: String, pay: gtxn.PaymentTransaction, asset: Asset) -> None:
-        assert not Global.current_application_address.is_opted_in(asset)
+    def allow_access(self, name: String, mbr_pay: gtxn.PaymentTransaction, asset: Asset) -> None:
+        # assert not Global.current_application_address.is_opted_in(asset), "asset already exists"
         
-        assert pay.receiver == Global.current_application_address
-        assert pay.amount == Global.asset_opt_in_min_balance
+        # assert mbr_pay.receiver == Global.current_application_address
+        # assert mbr_pay.amount == Global.asset_opt_in_min_balance
 
         itxn.AssetTransfer(
             xfer_asset = asset,
@@ -30,12 +48,12 @@ class AdvMarketplace(ARC4Contract):
         # Keys -> Values
         # asset_id (unitary_price) -> amounts -> Box (storage) -> Bytes
     @abimethod()
-    def first_deposit(self, xfer: gtxn.AssetTransferTransaction, nonce: arc4.UInt64, unitary_price: arc4.UInt64, pay: gtxn.PaymentTransaction) -> None:
-        assert pay.sender == Txn.sender
-        assert pay.receiver == Global.current_application_address
-        assert pay.amount == MINIMUM_FEE_FOR_SALE
-        # create = 0.0025
-        # per byte = 0.0004
+    def first_deposit(self, xfer: gtxn.AssetTransferTransaction, nonce: arc4.UInt64, unitary_price: arc4.UInt64, mbr_pay: gtxn.PaymentTransaction) -> None:
+        assert mbr_pay.sender == Txn.sender
+        assert mbr_pay.receiver == Global.current_application_address
+        assert mbr_pay.amount == FOR_SALE_BOX_MBR
+        # create = 0.0025 algo
+        # per byte = 0.0004 algos
 
         # address + asset_id + nonce                                    arc4.Uint64 to calculate
         box_key = Txn.sender.bytes + op.itob(xfer.xfer_asset.id) + nonce.bytes
@@ -44,15 +62,15 @@ class AdvMarketplace(ARC4Contract):
         assert not exists
 
         assert xfer.sender == Txn.sender
-        assert xfer.receiver == Global.current_application_address
-        assert xfer.amount > 0
-
+        assert xfer.asset_receiver == Global.current_application_address
+        assert xfer.asset_amount > 0
+        # deposited, selling price, bidder, bid quantity, bid price
         # address (32) + UInt (8) + UInt (8) -> amount (8) + unitary_price (8)
         # 48 -> 16 = 64
-        assert op.Box.create(box_key, BOX_LENGTH_FOR_SALE) # assert tạo box -> 64 bit, if valid then storage
+        assert op.Box.create(box_key, FOR_SALE_BOX_VALUE_LENGTH) # assert tạo box -> 64 bit, if valid then storage
         op.Box.replace(box_key, 0, op.itob(xfer.asset_amount) + unitary_price.bytes)
-        # a: address; b: offset (position); c: value
-        # hash -> key -> value
+        # a: address; b: offset (position); c: value (bytes)
+        # ~ hash -> key -> value
         
         # global state
     @abimethod
@@ -62,34 +80,67 @@ class AdvMarketplace(ARC4Contract):
         assert exists
 
         assert xfer.sender == Txn.sender
-        assert xfer.receiver == Global.current_application_address
-        assert xfer.amount > 0
+        assert xfer.asset_receiver == Global.current_application_address
+        assert xfer.asset_amount > 0
 
         # 100 -> tokens
         # 50 -> tokens (overiding) therefore need to plus current_amount
-        current_amount = op.btoi(op.Box.extract(box_key, 0, 8)) # storage int (by convert byte into int)
-        op.Box.replace(box_key, 0, op.itob(xfer.asset_amount + current_amount)) # convert int into byte and storage
+        current_deposited = op.btoi(op.Box.extract(box_key, 0, 8)) # storage int (by convert byte into int)
+        op.Box.replace(box_key, 0, op.itob(current_deposited + xfer.asset_amount)) # convert int into byte and storage
 
     @abimethod
-    def set_price(self, asset_id : UInt64, nonce: arc4.UInt64, unitary_price: arc4.UInt64) -> None:
-        box_key = Txn.sender.bytes + op.itob(asset_id) + nonce.bytes
+    def set_price(self, asset : UInt64, nonce: arc4.UInt64, unitary_price: arc4.UInt64) -> None:
+        box_key = Txn.sender.bytes + op.itob(asset) + nonce.bytes
 
         op.Box.replace(box_key, 8, unitary_price.bytes)
         # int 8 + 8 =16; bytes: 8 + 8 = 88
 
+    @abimethod
+    def withdraw(self, asset: Asset, nonce: arc4.UInt64) -> None:
+        box_key = Txn.sender.bytes + op.itob(asset.id) + nonce.bytes
+        
+        current_deposit = op.btoi(op.Box.extract(box_key, 0, 8))
+        current_bidder = Account(op.Box.extract(box_key, 16, 32))
+        if current_bidder != Global.zero_address:
+            current_bid_deposit = self.quantity_price(
+                op.btoi(op.Box.extract(box_key, 48, 8)),
+                op.btoi(op.Box.extract(box_key, 56, 8)),
+                asset.decimals,   
+            )
+            itxn.Payment(
+                receiver = current_bidder,
+                amount = current_bid_deposit,
+                fee = 0
+            ).submit()
+
+        _delete = op.Box.delete(box_key)
+
+        itxn.Payment(
+            receiver = Txn.sender,
+            amount = FOR_SALE_BOX_MBR,
+            fee = 0
+        ).submit()
+
+        itxn.AssetTransfer(
+            xfer_asset = asset,
+            asset_receiver = Txn.sender,
+            asset_amount = current_deposit,
+            fee = 0,
+        ).submit()
+
     # retrancy attacks
     @abimethod
-    def buy(self, buyer: arc4.Address, asset: Asset, nonce: arc4.UInt64, buyer_txn: gtxn.PaymentTransaction, quantity: UInt64) -> None:
-        box_key = Txn.sender.bytes + op.itob(asset.id) + nonce.bytes
+    def buy(self, owner: arc4.Address, asset: Asset, nonce: arc4.UInt64, buy_pay: gtxn.PaymentTransaction, quantity: UInt64) -> None:
+        box_key =  owner.bytes + op.itob(asset.id) + nonce.bytes
         current_unitary_price = op.btoi(op.Box.extract(box_key, 8, 8))
        
-        amount_to_paid = self._quantity_price(
+        amount_to_be_paid = self.quantity_price(
             quantity, current_unitary_price, asset.decimals
         )
         
-        assert buyer_txn.sender == Txn.sender
-        assert buyer_txn.receiver == buyer
-        assert buyer_txn.amount == amount_to_paid
+        assert buy_pay.sender == Txn.sender
+        assert buy_pay.receiver.bytes == owner.bytes
+        assert buy_pay.amount == amount_to_be_paid
 
         # 16 decimals
         # 6 decimals = 1000
@@ -98,8 +149,8 @@ class AdvMarketplace(ARC4Contract):
 
         # 1000 tokens -> transfer 100
 
-        current_amount = op.btoi(op.Box.extract(box_key, 0, 8))
-        op.Box.replace(box_key, 0, op.itob(current_amount - quantity))
+        current_deposited = op.btoi(op.Box.extract(box_key, 0, 8))
+        op.Box.replace(box_key, 0, op.itob(current_deposited - quantity))
 
         itxn.AssetTransfer(
             xfer_asset = asset,
@@ -109,23 +160,11 @@ class AdvMarketplace(ARC4Contract):
         ).submit()  
         # newtokens = currentTokens - tokensTransfer
 
-    @subroutine
-    def _quantity_price(self, quantity: UInt64, price: UInt64, asset_decimals: UInt64) -> UInt64:
-        amount_scaled_high, amount_scaled_low = op.mulw(price, quantity) # chia
-        scalling_high, scalling_low = op.expw(10, asset_decimals) # 10^8 => 100_000_000
-        _a,amount_to_paid,_remainder_high,_remainder_low = op.divmodw(
-            amount_scaled_high,
-            amount_scaled_low,
-            scalling_high,
-            scalling_low
-        )
-        assert not _a
-
-        return amount_to_paid
     # start to bid
     @abimethod
-    def bid(self, pay: gtxn.PaymentTransaction, asset: Asset, nonce: arc4.UInt64, owner: arc4.Address, unitary_price: UInt64, quantity: arc4.UInt64) -> None:
-        assert Txn.sender.is_opted_in(asset)                            #unitary_price: số tiền to bid
+    def bid(self, bid_pay: gtxn.PaymentTransaction, asset: Asset, nonce: arc4.UInt64, owner: arc4.Address, unitary_price: arc4.UInt64, quantity: arc4.UInt64) -> None:
+                                                                                    #unitary_price: số tiền to bid
+        assert Txn.sender.is_opted_in(asset)                            
         box_key = owner.bytes + op.itob(asset.id) + nonce.bytes
         
         # 0 -> 16 ( amount, unitary_price ) (storaged)
@@ -135,23 +174,23 @@ class AdvMarketplace(ARC4Contract):
             current_bid_unitary_price = op.btoi(op.Box.extract(box_key, 56, 8))
             assert unitary_price > current_bid_unitary_price
             # 1000 -> previous bidder
-            current_bid_amount = self._quantity_price(
+            current_bid_amount = self.quantity_price(
                 current_bid_quantity, current_bid_unitary_price, asset.decimals
             )
         
-        itxn.Payment(
-            receiver = current_bidder,
-            amount = current_bid_amount,
-            fee = 0
-        ).submit()
-        amount_to_paid = self._quantity_price(
+            itxn.Payment(
+                receiver = current_bidder,
+                amount = current_bid_amount,
+                fee = 0
+            ).submit()
+        amount_to_be_bid = self.quantity_price(
             quantity.native, 
-            unitary_price,
+            unitary_price.native,
             asset.decimals 
         )
-        assert pay.sender == Txn.sender
-        assert pay.receiver == Global.current_application_address
-        assert pay.amount == amount_to_paid
+        assert bid_pay.sender == Txn.sender
+        assert bid_pay.receiver == Global.current_application_address
+        assert bid_pay.amount == amount_to_be_bid
 
         op.Box.replace(box_key, 16, Txn.sender.bytes + quantity.bytes + unitary_price.bytes)
 
@@ -159,60 +198,28 @@ class AdvMarketplace(ARC4Contract):
     def accept_bid(self, asset: Asset, nonce: arc4.UInt64) -> None:
         box_key = Txn.sender.bytes + op.itob(asset.id) + nonce.bytes
 
-        winner = Account(op.Box.extract(box_key, 16, 32)) # address -> 32 bytes
-            
-        highest_bid_quantity = op.btoi(op.Box.extract(box_key, 48, 8))
-        highest_bid_unitary_price = op.btoi(op.Box.extract(box_key, 56, 8))
-            
-        current_quantity = op.Box.extract(box_key, 48, 8)
-        current_unitary_price = op.Box.extract(box_key, 8, 8)
+        best_bidder = Account(op.Box.extract(box_key, 16, 32)) # address -> 32 bytes
+        assert best_bidder != Global.zero_address
 
-        assert current_unitary_price > highest_bid_unitary_price
+        best_bid_quantity = op.btoi(op.Box.extract(box_key, 48, 8))
+        best_bid_unitary_price = op.btoi(op.Box.extract(box_key, 56, 8))        
+        current_deposited = op.btoi(op.Box.extract(box_key, 48, 8))
+        current_unitary_price = op.btoi(op.Box.extract(box_key, 8, 8))
+
+        assert current_unitary_price > best_bid_unitary_price
             
         # min_quantity
-        min_quantity = current_quantity if current_quantity < highest_bid_quantity else highest_bid_quantity
+        min_quantity = current_deposited if current_deposited < best_bid_quantity else best_bid_quantity
         # 10_000 -> quantity -> 9000
-        highest_bid_amount = self._quantity_price(
-            min_quantity, highest_bid_unitary_price, asset.decimals
+        best_bid_amount = self.quantity_price(
+            min_quantity, best_bid_unitary_price, asset.decimals
         )
 
-        op.Box.replace(box_key, 0, op.itob(current_quantity - min_quantity )) # 9_000 - 9_000
-        op.Box.replace(box_key, 48, op.itob(highest_bid_quantity - min_quantity)) # 10_000 - 9000 = 100
-
         itxn.Payment(
             receiver = Txn.sender,
-            amount = highest_bid_amount,
-            free = 0
-        ).submit()
-    
-    @abimethod
-    def withdraw(self, asset: Asset, nonce: arc4.UInt64) -> None:
-        box_key = Txn.sender.bytes + op.itob(asset.id) + nonce.bytes
-        
-        current_bidder = Account(op.Box.extract(box_key, 16, 32))
-        current_deposit = op.btoi(op.Box.extract(box_key, 0, 8))
-        if current_bidder != Global.zero_address:
-            current_bid_remainder = self._quantity_price(
-                op.btoi(op.Box.extract(box_key, 48, 8)),
-                op.btoi(op.Box.extract(box_key, 56, 8)),
-                asset.decimals       
-            )
-            itxn.Payment(
-                receiver = current_bidder,
-                amount = current_bid_remainder,
-                fee = 0
-            ).submit()
-
-        _delete = op.Box.delete(box_key)
-
-        itxn.Payment(
-            receiver = Txn.sender,
-            amount = MINIMUM_FEE_FOR_SALE,
+            amount = best_bid_amount,
             fee = 0
         ).submit()
 
-        itxn.AssetTransfer(
-            xfer_asset = asset,
-            asset_receiver = Txn.sender,
-            asset_amount = current_deposit
-        ).submit()
+        op.Box.replace(box_key, 0, op.itob(current_deposited - min_quantity )) # 9_000 - 9_000
+        op.Box.replace(box_key, 48, op.itob(best_bid_quantity - min_quantity)) # 10_000 - 9000 = 100
--- adv-marketplace/projects/adv-marketplace-contracts/tests/adv_marketplace_test.py
+++ adv-marketplace/projects/adv-marketplace-contracts/tests/adv_marketplace_test.py
@@ -1,6 +1,10 @@
 from collections.abc import Generator
-
+from algokit_utils.beta.algorand_client import AlgorandClient, PayParams
+from algokit_utils.beta.composer import TransactionWithSigner
+from algopy import Bytes, UInt64, arc4
+import algopy
 import pytest
+from algopy_testing.arc4 import Address 
 from algopy_testing import AlgopyTestContext, algopy_testing_context
 
 from smart_contracts.adv_marketplace.contract import AdvMarketplace
@@ -12,14 +16,17 @@ def context() -> Generator[AlgopyTestContext, None, None]:
         yield ctx
         ctx.reset()
 
+def test_allow_asset(context: AlgopyTestContext) -> None:
+    # mbbr_pay: minimun pay, asset
+    sender = context.any_account(); 
+    contract = AdvMarketplace();
+    contract.__init__();
+    asset = context.any_asset(creator = sender, decimals = UInt64(3), total = UInt64(10_000_000))
 
-def test_hello(context: AlgopyTestContext) -> None:
-    # Arrange
-    dummy_input = context.any_string()
-    contract = AdvMarketplace()
-
-    # Act
-    output = contract.hello(dummy_input)
-
-    # Assert
-    assert output == f"Hello, {dummy_input}"
+    app_address = context.default_application.address
+    pay_txn = context.any_payment_transaction(sender = sender, receiver = app_address, amount = UInt64(0))
+    # input payment Txn
+    contract.allow_access(mbr_pay = pay_txn, asset = asset)
+    itxn_txn = context.last_submitted_itxn.payment
+    # Comparing Txn
+    assert itxn_txn.sender == sender
